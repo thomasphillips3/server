4c2b311289c42d1adad05b003bec3ae2ac514b93
Thomas Phillips <tpphilli@amazon.com>
2016-02-24 23:29:27 -0500

    Refactored 'Hello World' code to do something real

--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 var http = require('http');
 
-http.createServer(function(request, response) {
-  response.writeHead(200, {"Content-Type": "text/plain"});
-  response.write('Cold World');
-  response.end();
-}).listen(8888);
+function onRequest(request, response) {
+  console.log('Request received.');
+  response.writeHead(200, {'Content-Type': 'text/plain'});
+  response.write('Colde World');
+  response.end;
+}
+
+http.createServer(onRequest).listen(8888);
+
+console.log('We on');
