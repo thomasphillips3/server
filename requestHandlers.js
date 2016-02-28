4de1b519aee7a58a5d25e1b208eccab9bd84fcf4
Thomas Phillips <tpphilli@amazon.com>
2016-02-28 02:37:41 -0800

    Added upload form to page Added logic to display uploaded image.

--- requestHandlers.js
+++ requestHandlers.js
@@ -1,32 +1,48 @@
-var querystring = require('querystring');
-var fs = require('fs');
+var querystring = require('querystring'),
+    fs = require("fs"),
+    formidable = require('formidable');
 
 function start(response) {
-  console.log('Request handler \'start\' was called.');
+  console.log("Request handler 'start' was called.");
 
-  var body = '<html>' +
-  '<head>' +
-  '<meta http-equiv="Content-Type" content="text/html; ' +
-  'charset=UTF-8" />' +
-  '</head>' +
-  '<body>' +
-  '<form action="/upload" method="post">' +
-  '<textarea name="text" rows="20" cols="60"></textarea>' +
-  '<input type="submit" value="Submit text" />' +
-  '</form>' +
-  '</body>' +
-  '</html>';
+  var body = '<html>'+
+    '<head>'+
+    '<meta http-equiv="Content-Type" '+
+    'content="text/html; charset=UTF-8" />'+
+    '</head>'+
+    '<body>'+
+    '<form action="/upload" enctype="multipart/form-data" '+
+    'method="post">'+
+    '<input type="file" name="upload" multiple="multiple">'+
+    '<input type="submit" value="Upload file" />'+
+    '</form>'+
+    '</body>'+
+    '</html>';
 
   response.writeHead(200, {'Content-Type': 'text/html'});
   response.write(body);
   response.end();
 }
 
-function upload(response, postData) {
+function upload(response, request) {
   console.log("response handler 'upload' was called.");
-  response.writeHead(200, {'Content-Type': 'text/plain'});
-  response.write('You said ' + querystring.parse(postData).text);
-  response.end();
+
+  var form = new formidable.IncomingForm();
+  console.log('Parsing incoming file');
+  form.parse(request, function(error, fields, files) {
+    console.log('Done parsing file');
+
+    fs.rename(files.upload.path, "tmp/test.png", function(error) {
+      if (error) {
+        fs.unlink('tmp/test.png');
+        fs.rename(files.upload.path, 'tmp/test.png');
+      }
+    });
+    response.writeHead(200, {'Content-Type': 'text/html'});
+    response.write("Received image: <br/>");
+    response.write("<img src='/show' />");
+    response.end();
+  });
 }
 
 function show(response) {
