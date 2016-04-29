5f9ba70d244c838fe96bad064233dd390a35d9a4
Thomas Phillips <thomasphillips3@gmail.com>
2016-04-29 15:01:23 -0700

    Added basic server functionality to app.js * Respond to root url * Respond to page not found (404)

---
+++ app.js
@@ -0,0 +1,19 @@
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(request, response) {
+  console.log('client request URL: ', request.url);
+  if (request.url === '/') {
+    fs.readFile('index.html', 'utf8', function(errors, contents) {
+      response.writeHead(200, {'Content-Type': 'text/html'});
+      response.write(contents);
+      response.end();
+    });
+  }
+  else {
+    response.writeHead(404);
+    response.end('File not found');
+  }
+});
+server.listen(6789);
+console.log('Running on port 6789');
