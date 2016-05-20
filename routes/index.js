b21837e3c26cf5cd33bc80f5b722dcf4aeaa9cb0
Thomas Phillips <thomasphillips3@gmail.com>
2016-05-19 22:08:43 -0700

    Fixed typos to get this sumbitch working

--- routes/index.js
+++ routes/index.js
@@ -3,13 +3,13 @@ module.exports = function Route(app){
     response.render('index');
   })
 
-  app.post('/result', function(response, request){
+  app.post('/result', function(request, response){
     submitted_info = {
       name: request.body.name,
       location: request.body.dojo_location,
       favLang: request.body.favorite_language,
       comment: request.body.comment
     };
-    response.render('result', {user_data: submitted_info});
+    response.render('results', {user_data: submitted_info});
   })
 };
--- server.js
+++ server.js
@@ -5,13 +5,11 @@ var bodyParser = require('body-parser');
 var app = express();
 app.use(bodyParser.urlencoded());
 app.use(express.static(path.join(__dirname, './static')));
-
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
-
 
 var route = require('./routes/index.js')(app);
 
 app.listen(9999, function(){
-  console.log("port 9999");
-})
+  console.log('port 9999');
+});
