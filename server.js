0f4b48cd64340f30d13c9bc9b844b64111d8d780
Thomas Phillips <thomasphillips3@gmail.com>
2016-05-20 13:55:18 -0700

    Fixed path

--- server.js
+++ server.js
@@ -4,7 +4,7 @@ var path = require('path');
 var app = express();
 
 app.use(express.static(path.join(__dirname + './static')));
-app.set('views', path.join(__dirname + './views'));
+app.set('views', path.join(__dirname + '/views'));
 app.set('view engine', 'ejs');
 
 app.get('/', function(request, response) {
